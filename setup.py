2c9787734e59a5bb83830bfa3a0418e4afe2b10f
Germán Cevallos <gcevallosmeneses@gmail.com>
2025-06-15 17:38:32 +0200

    Modificacion setup

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="calculadora-matematica",
+    name="temporizador",
     version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[],
     author="German Andres Cevallos",
     author_email="germanandrescevallos@gmail.com",
-    description="Una librería de calculadoras matemáticas",
+    description="Una librería para manejo de temporizadores",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/gercevas/Proyecto_git.git",
